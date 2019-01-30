c28a47cee0dcf039e430163aa0c567b0cad3c540
Shivali <shivali@inbe.com>
2019-01-30 15:14:28 +0530

    sound according to frequencies

--- android/src/main/java/com/frequency/sounds/Module.java
+++ android/src/main/java/com/frequency/sounds/Module.java
@@ -9,10 +9,22 @@ import com.facebook.react.bridge.ReactMethod;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.os.*;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+
 public class Module extends ReactContextBaseJavaModule {
 
   private static final String DURATION_SHORT_KEY = "SHORT";
   private static final String DURATION_LONG_KEY = "LONG";
+
+  private final int duration = 3; // seconds
+  private final int sampleRate = 8000;
+  private final int numSamples = duration * sampleRate;
+  private final double sample[] = new double[numSamples];
+  private final double freqOfTone = 220; // hz
+  private final byte generatedSnd[] = new byte[2 * numSamples];
 
   public Module(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -31,8 +43,35 @@ public class Module extends ReactContextBaseJavaModule {
     return constants;
   }
 
+  void playSound(){
+    final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+           sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+           AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
+           AudioTrack.MODE_STATIC);
+    audioTrack.write(generatedSnd, 0, generatedSnd.length);
+    audioTrack.play();
+   }
+
+  void genTone(double frequency){
+    for (int i = 0; i < numSamples; ++i) {
+        sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/frequency));
+    }
+    int idx = 0;
+    for (final double dVal : sample) {
+        // scale to maximum amplitude
+        final short val = (short) ((dVal * 32767));
+        // in 16 bit wav PCM, first byte is the low order byte
+        generatedSnd[idx++] = (byte) (val & 0x00ff);
+        generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+
+    }
+  }
+
   @ReactMethod
-  public void show(String message, int duration) {
-    Toast.makeText(getReactApplicationContext(), message, duration).show();
+  public void show(double frequency) {
+    // Toast.makeText(getReactApplicationContext(), message, duration).show();
+    genTone(frequency);
+    playSound();
+
   }
 }
